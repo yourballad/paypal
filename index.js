eb722334105fe987827dfabea7fad9de00651063
devie1997 <dmoodley1997@gmail.com>
2025-05-31 12:11:57 -0700

    updated the Auth rouths

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Auth Routes (after JSON middleware)
 const authRoutes = require('./routes/auth');
-app.use('/api/auth', authRoutes);
-//app.use('/', authRoutes);
+app.use('/', authRoutes);
 // Serve login page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
